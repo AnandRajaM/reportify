7dd561bd5113a596828dfbe37197d41bc37c87f2
aa <142321494+AnandRajaM@users.noreply.github.com>
2025-12-12 21:30:26 +0530

    added pdf generator and samples

---
+++ reportGen/data_fetching.py
@@ -0,0 +1,70 @@
+#Imports
+from openai import OpenAI
+import google.generativeai as genai
+import pymongo
+from pymongo import MongoClient
+
+
+
+#Client creations & tokens
+genai.configure(api_key = "<api-key>")
+model = genai.GenerativeModel('gemini-pro')
+
+
+#MongoDB connection & data fetching
+def get_patient_docs(customer_name,booking_date):
+    cluster = MongoClient("<mogodb-connection>")
+    db = cluster['RedCliffe_Labs']
+    collection = db['patient_details']
+    # Construct query
+    query = {
+        "$and": [
+            {"customer_name": customer_name},
+            {"booking_date": booking_date},
+        ]
+    }
+    # Find documents matching the query
+    result = collection.find(query)
+    booking_id = None
+    patient_tests = []
+    # Iterate over the result
+    for doc in result:
+        booking_id = doc['booking_id']
+        patient_tests.append((doc['test_name'], doc['test_values']))
+        
+    return patient_tests,booking_id
+
+
+
+#Medical data fetching
+def get_data(test_name):  
+    response = model.generate_content("Give me a summary of this test in 30 words only : "+test_name)
+    return response.text.strip()
+
+
+def get_data_cause(test_name, high_low_normal): 
+    response = model.generate_content(f"Generate 3 possible causes for {test_name} {high_low_normal} result . Each cause should be 10 words or less and should start with a index number.")
+    return response.text.strip().split('\n')
+    
+def get_data_cause_para(test_name, high_low): 
+    response = model.generate_content(f"Give me a general paragraph about {test_name} {high_low} result in only 30 words or less.")
+    return response.text.strip()
+
+
+def get_data_consider(test_name,high_low):
+    response = model.generate_content(f"What are the recommended next steps for {test_name} {high_low} results? Please provide concise guidance within 50 words and do not provide causes.")
+    return response.text.strip()
+
+
+def merge_lists(existing_list, new_list):
+    for new_item in new_list:
+        found = False
+        for existing_item in existing_list:
+            if existing_item[0] == new_item[0]:
+                existing_item[1].append(new_item[1])
+                found = True
+                break
+        if not found:
+            existing_list.append([new_item[0], [new_item[1]]])
+        
+    return existing_list
---
+++ reportGen/pdf_generator.py
@@ -0,0 +1,77 @@
+import reportlab
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfbase import pdfmetrics
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.platypus import Paragraph
+from reportlab.graphics import shapes
+from reportlab.graphics.shapes import Polygon
+
+import PyPDF2
+
+# Importing custom modules
+from data_fetching import *
+from pages import *
+
+
+# Customer name and booking date to generate report
+customer_name = "Sankar Roy"
+booking_date= "2023-11-01 00:00:00 UTC"
+
+
+# Fetch patient documents and booking ID
+patient_docs , booking_id = get_patient_docs(customer_name,booking_date)
+test_names = [(item[0],) for item in patient_docs]
+
+
+# Initialize lists to store highlighted tests and report pages
+highlighted_tests=[]
+Report_pages = []
+c=0
+
+
+# Iterate through each diagnostic test in patient documents
+for diagnostic_test in patient_docs:
+    c+=1
+    if diagnostic_test[0]=="Complete Blood Count (CBC)":
+        filename = f"./stack_pdfs/report_page_{c}.pdf"
+        print("RBC"+str(c)) 
+        rbc_highlighted_tests = create_rbc_report_page(filename, A4, "./Resources/rbc_page.png", diagnostic_test,booking_id)
+        # Merge highlighted tests into the main list
+        highlighted_tests = merge_lists(highlighted_tests,rbc_highlighted_tests)
+        # Append report page filename to the list
+        Report_pages.append(filename)    
+
+    elif diagnostic_test[0] == "Urine Routine and Microscopic Examination":
+        filename = f"./stack_pdfs/report_page_{c}.pdf"
+        print("URME"+str(c))
+        urme_highlighted_tests=create_urme_report_page(filename, A4, "./Resources/urme_page.png", diagnostic_test,booking_id)
+        # Merge highlighted tests into the main list
+        highlighted_tests = merge_lists(highlighted_tests,urme_highlighted_tests)
+        # Append report page filename to the list
+        Report_pages.append(filename)
+    
+    else:
+        print("val"+str(c))
+        filename = f"./stack_pdfs/report_page_{c}.pdf"
+        highlighted = create_report_page(filename, A4, "./Resources/2tests_report_page.png", diagnostic_test,booking_id)
+        # Merge highlighted tests into the main list
+        highlighted_tests = merge_lists(highlighted_tests,highlighted)
+        # Append report page filename to the list
+        Report_pages.append(filename)
+
+# Create summary report with body image- page
+create_body_report_page("./stack_pdfs/summary_report_page.pdf", A4, "./Resources/summary.png", highlighted_tests,test_names,booking_id)
+Report_pages.insert(0, "./stack_pdfs/summary_report_page.pdf")
+
+# Create cover page with customer name
+create_cover_pdf("./stack_pdfs/coverpage.pdf", letter, "./Resources/cover.png", customer_name)
+Report_pages.insert(0, "./stack_pdfs/coverpage.pdf")
+
+print("Final report created successfully.")
+
+
+
